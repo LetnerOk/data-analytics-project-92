976132816ccba28de49c5f9f291d74c3da7790da
LetnerOk <135754081+LetnerOk@users.noreply.github.com>
2023-07-03 16:09:42 +0300

    Update queries step6.sql

--- queries.sql
+++ queries.sql
@@ -71,3 +71,45 @@ SELECT
 FROM weekday_income
 ORDER BY num_weekday, name
 ;
+
+-- Count of customers in different age groups
+SELECT 
+	CASE 
+		WHEN age BETWEEN 16 AND 25 THEN '16-25'
+	    WHEN age BETWEEN 26 AND 40 THEN '25-40'
+	    WHEN  age > 40 THEN '40+'
+	END age_category,
+	COUNT(customer_id) AS count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category
+;
+
+--Count of customers and their income by months
+SELECT
+	TO_CHAR(sale_date, 'YYYY-MM') AS date,
+	COUNT(DISTINCT s.customer_id) AS total_customers,
+	SUM(s.quantity*p.price) AS income
+FROM sales s
+INNER JOIN products p
+ON s.product_id = p.product_id
+GROUP BY 1
+ORDER BY 1
+;
+
+--customers the first purchase came at the time of the sale of special promotions (price is 0)
+SELECT 
+	DISTINCT ON (s.customer_id)
+	CONCAT(c.first_name, ' ', c.last_name) AS customer,
+	s.sale_date,
+	CONCAT(e.first_name, ' ', e.last_name) AS seller
+	FROM sales s
+INNER JOIN employees e
+ON s.sales_person_id = e.employee_id
+INNER JOIN customers c
+ON s.customer_id = c.customer_id
+INNER JOIN products p
+ON s.product_id = p.product_id
+WHERE price = 0
+ORDER BY s.customer_id, s.sale_date
+;
