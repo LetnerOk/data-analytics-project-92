b416c735389dfcd31c94bea8b244bb23a9bdd09e
LetnerOk <135754081+LetnerOk@users.noreply.github.com>
2023-07-07 20:07:35 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -19,34 +19,26 @@ LIMIT 10
 ;
 
 -- The list of employees whose income is below the average income of all employees
-WITH income AS
+WITH total_avg_val AS
 (
-	SELECT
-		CONCAT(first_name, ' ', last_name) AS name,
-		COUNT(sales_id) AS operations,
-		SUM(price*quantity) AS income
-	FROM employees e
-	INNER JOIN sales s
-		ON e.employee_id = s.sales_person_id
-	INNER JOIN products P
+	SELECT 
+	AVG(price*quantity) AS total_avg
+	FROM sales s
+	INNER JOIN products p
 	ON p.product_id = s.product_id
-	GROUP BY 1
-	ORDER BY 3
-),
- total_average_income AS 
- (
-	SELECT
-		name,
-		income/operations AS average_income,
-		(SELECT SUM(income)/SUM(operations) FROM income) AS total_avg
-	FROM income
- )
+)
+
 SELECT
-	name,
-	ROUND(average_income, 0) AS average_income
-FROM total_average_income
-WHERE average_income < total_avg
-ORDER BY average_income
+	CONCAT(first_name, ' ', last_name) AS name,
+	ROUND(AVG(price*quantity), 0) AS average_income
+FROM total_avg_val tav, employees e
+INNER JOIN sales s
+	ON e.employee_id = s.sales_person_id
+INNER JOIN products P
+	ON p.product_id = s.product_id
+GROUP BY 1, tav.total_avg
+HAVING AVG(price*quantity) < tav.total_avg
+ORDER BY 2 
 ;
 
 --income data for each employee and day of the week
